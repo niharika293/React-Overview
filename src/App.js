1082dc436c2f6c87e9405164ecdedbf9b28cbd2c
Niharika Gurnani <niharikagurnani13@gmail.com>
2021-03-31 14:54:11 +0530

    Create a component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+// Components can be functions, classes.
+// Restrictions on Keywords : class -> className, for -> htmlFor
+// JSX expressions must return single parent element.
+// To use header component, import & embed like an XML Tag. 
+// For class based components, important to have {import react from react}
 
+import React from 'react'
+import Header from './Components/Header'
 function App() {
+  const x = false;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello From React</h1>
+      <h2>Hello {x?'Yes':'No'}</h2>
+      <Header/>
     </div>
   );
 }
+// Class Based Component
+// class App extends React.Component{
+//   render(){
+//     return <h1>Hello from class!!</h1>
+//   }
+// }
 
 export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+// When using React, think of your UI as a bunch of separate components.
+// Components render/ return JSX.
+
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Task Tracker</h1>
+        </div>
+    )
+}
+
+export default Header
